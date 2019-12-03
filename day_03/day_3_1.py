ad9ccf664e375d43f83ce35146b182f0215c43bf
Anders Eriksson <aeri3@kth.se>
2019-12-03 18:24:55 +0100

    solve day 3 pt 1

---
+++ day_03/day_3_1.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from collections import defaultdict
+
+dirs = {"U": (0, 1), "R": (1, 0), "D": (0, -1), "L": (-1, 0)}
+
+# Wire 1
+wire1 = set()
+pos = (0, 0)
+for seg in stdin.readline().split(","):
+    dir = dirs[seg[0]]
+    length = int(seg[1:])
+
+    for _ in range(length):
+        pos = (pos[0] + dir[0], pos[1] + dir[1])
+        wire1.add(pos)
+
+# Wire 2
+pos = (0, 0)
+intersections = []
+for seg in stdin.readline().split(","):
+    dir = dirs[seg[0]]
+    length = int(seg[1:])
+    
+    for _ in range(length):
+        pos = (pos[0] + dir[0], pos[1] + dir[1])
+        if pos in wire1:
+            intersections.append(pos)
+
+print(min([abs(x) + abs(y) for x, y in intersections]))
