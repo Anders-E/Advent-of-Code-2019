d4a37d58057d247c6a4e3f403f417622367a0cea
Anders Eriksson <aeri3@kth.se>
2019-12-08 18:37:25 +0100

    remove unnecessary semicolon

--- day_08/day_8_2.hs
+++ day_08/day_8_2.hs
@@ -2,6 +2,6 @@ import Data.List
 import Data.List.Split
 import Data.Char
 
-main = interact (unlines . map (map (\c -> if c == 0 then ' '; else '#')) . decode . map digitToInt)
+main = interact (unlines . map (map (\c -> if c == 0 then ' ' else '#')) . decode . map digitToInt)
 
 decode xs = chunksOf 25 . map (head . dropWhile (==2)) . transpose $ chunksOf (25 * 6) xs
