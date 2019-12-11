9c80de8ff762a24f6ad479473f14389da8e55c3d
Anders Eriksson <aeri3@kth.se>
2019-12-11 01:03:32 +0100

    minor day 10 pt 1 edits

--- day_10/day_10_1.hs
+++ day_10/day_10_1.hs
@@ -2,10 +2,10 @@ import qualified Data.Set as S
 
 main = interact ((++ "\n") . show . maximum . monitoringStations . readAsteroids)
 
-monitoringStations asteroids = map (monitoringStation asteroids) asteroids
+monitoringStations asteroids = S.map (monitoringStation asteroids) asteroids
 monitoringStation asteroids origin  = length dirs
   where
-    dirs = S.fromList . map direction $ map (flip subTup origin) (filter (/= origin) asteroids)
+    dirs = S.map direction $ S.map (flip subTup origin) (S.delete origin asteroids)
 
 -- Vectors
 subTup (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
@@ -14,4 +14,4 @@ direction (x, y) = (x `div` (gcd x y), y `div` (gcd x y))
 -- Input
 width = 33
 coords = zip (cycle [0..width-1]) $ map (flip div width) [0..]
-readAsteroids s = map fst . filter (\(_, c) -> c /= '.') . zip coords $ filter (/='\n') s
+readAsteroids s = S.fromList . map fst . filter (\(_, c) -> c /= '.') . zip coords $ filter (/='\n') s
